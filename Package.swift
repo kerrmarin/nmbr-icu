444a02bddb66e3fb69adc18444be347ff158fc38
Kerr Marin Miller <kerr@kerrmarin.com>
2024-07-11 08:38:22 +0100

    Remove xcodeproj

--- Package.swift
+++ Package.swift
@@ -6,21 +6,17 @@ import PackageDescription
 let package = Package(
     name: "nmbr-icu",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "nmbr-icu",
-            targets: ["nmbr-icu"]),
+        .library(name: "nmbr-icu", targets: ["nmbr-icu"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "nmbr-icu",
-            dependencies: ["ICU"]),
-        .binaryTarget(name: "ICU", path: "ICU.xcframework"),
-        .testTarget(
-            name: "nmbrTests",
-            dependencies: ["nmbr-icu"]),
+        // Main NMBR target, contains 1 ObjC++ file (h + mm)
+        .target(name: "nmbr-icu",
+                dependencies: ["ICU"]),
+        // Target to build the ICU framework, an xcframework for the ICU C++ code
+        .binaryTarget(name: "ICU",
+                      path: "./ICU.xcframework"),
+        // Test target
+        .testTarget(name: "nmbrTests", dependencies: ["nmbr-icu"])
     ],
     cxxLanguageStandard: .cxx20
 )
